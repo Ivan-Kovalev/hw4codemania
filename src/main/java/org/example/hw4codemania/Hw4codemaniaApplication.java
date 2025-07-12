3ab8118da970d30cc2d41aaba946f9239785b4df
Ivan-Kovalev <gangrenresdwa@gmail.com>
2025-07-12 21:44:47 +0300

    master

---
+++ src/main/java/org/example/hw4codemania/Hw4codemaniaApplication.java
@@ -0,0 +1,13 @@
+package org.example.hw4codemania;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw4codemaniaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw4codemaniaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/hw4codemania/Hw4codemaniaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.hw4codemania;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw4codemaniaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
