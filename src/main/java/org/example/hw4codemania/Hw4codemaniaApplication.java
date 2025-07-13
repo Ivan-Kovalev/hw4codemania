70e72157c92cd58456a81dcefa40e16ca02ab0fc
Ivan-Kovalev <gangrenresdwa@gmail.com>
2025-07-13 11:56:44 +0300

    hello world

--- src/main/java/org/example/hw4codemania/Hw4codemaniaApplication.java
+++ src/main/java/org/example/hw4codemania/Hw4codemaniaApplication.java
@@ -1,13 +1,17 @@
 package org.example.hw4codemania;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.logging.Logger;
 
 @SpringBootApplication
 public class Hw4codemaniaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(Hw4codemaniaApplication.class, args);
+        LoggerFactory.getLogger(Hw4codemaniaApplication.class).info("Hello World!");
     }
 
 }
